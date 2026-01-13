686fd10c6bfd3d33fc76c5101805d2f0f7450435
bytemeprod <vavilov.main@icloud.com>
2026-01-13 19:33:16 +0300

    fix: add method log to middleware

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.listen(PORT, () => {
 })
 
 function loggerMiddleware(req, res, callback) {
-    console.log("New request:", req.pathname);
+    console.log("New request:", req.method, req.pathname);
     callback();
 }
